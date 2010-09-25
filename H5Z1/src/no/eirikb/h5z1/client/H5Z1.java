e8f509a9cc8dfabf4de855411ef58f331ba83fbf
eirikb <devnull@localhost>
2010-09-25 19:54:29 +0200

    wth

---
+++ H5Z1/src/no/eirikb/h5z1/client/H5Z1.java
@@ -0,0 +1,15 @@
+package no.eirikb.h5z1.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class H5Z1 implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("Hello world"));
+	}
+}
