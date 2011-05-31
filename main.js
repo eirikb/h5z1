7dd08d64d255dc8b4e4ea892ca332143aaf4f263
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-31 20:09:30 +0200

    Using the new utils-file, and building a world block

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
+
+var world = utils.createWorld();
+
+var circleSd = new box2d.CircleDef();
+circleSd.density = 1.0;
+circleSd.radius = 20;
+circleSd.restitution = 1.0;
+circleSd.friction = 0;
+
+var circleBd = new box2d.BodyDef();
+circleBd.AddShape(circleSd);
+circleBd.position.Set(200, 10);
+
+var circleBody = world.CreateBody(circleBd);
+
+var jointDef = new box2d.RevoluteJointDef();
+jointDef.anchorPoint.Set(250, 200);
+jointDef.body1 = world.GetGroundBody();
+jointDef.body2 = circleBody;
+world.CreateJoint(jointDef);
+
+var $canvas = $('<canvas>'),
+ctx = $canvas[0].getContext('2d');
+
 $(function() {
-    var worldAABB = new box2d.AABB();
-    worldAABB.minVertex.Set( - 1000, - 1000);
-    worldAABB.maxVertex.Set(1000, 1000);
-
-    var gravity = new box2d.Vec2(0, 300),
-    doSleep = true;
-
-    world = new box2d.World(worldAABB, gravity, doSleep);
-
-    circleSd = new box2d.CircleDef();
-    circleSd.density = 1.0;
-    circleSd.radius = 20;
-    circleSd.restitution = 1.0;
-    circleSd.friction = 0;
-
-    circleBd = new box2d.BodyDef();
-    circleBd.AddShape(circleSd);
-    circleBd.position.Set(200, 10);
-
-    circleBody = world.CreateBody(circleBd);
-
-    var jointDef = new box2d.RevoluteJointDef();
-    jointDef.anchorPoint.Set(250, 200);
-    jointDef.body1 = world.GetGroundBody();
-    jointDef.body2 = circleBody;
-    world.CreateJoint(jointDef);
-
-    var $canvas = $('<canvas>'),
-    ctx = $canvas[0].getContext('2d');
-
     $('body').append($canvas);
 
     $canvas.playground({
