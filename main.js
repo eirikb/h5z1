fe6cd5e7f654c60450a5a8fb12af29877af92654
Eirik Brandtzæg <eirikb@eirikb.no>
2011-06-02 13:55:46 +0200

    Better movement for player

--- main.js
+++ main.js
@@ -17,22 +17,23 @@ $(function() {
         userData: 'filled'
     });
 
-    var boxSd = new box2d.BoxDef();
-    boxSd.density = 1;
-    boxSd.friction = 0;
-    boxSd.extents.Set(10, 20);
-    var boxBd = new box2d.BodyDef();
-    boxBd.preventRotation = true;
-    boxBd.allowSleep = false;
-    boxBd.AddShape(boxSd);
-    boxSd = new box2d.BoxDef();
-    boxSd.density = 1;
-    boxSd.friction = 0;
-    boxSd.extents.Set(6, 2);
-    boxSd.localPosition.Set(1, 22);
-    boxBd.AddShape(boxSd);
-    boxBd.position.Set(150, 250);
-    player = world.CreateBody(boxBd);
+    var sd = new box2d.BoxDef();
+    sd.density = 1;
+    sd.friction = 0;
+    sd.extents.Set(10, 20);
+    var bd = new box2d.BodyDef();
+    bd.preventRotation = true;
+    bd.allowSleep = false;
+    bd.AddShape(sd);
+    sd = new box2d.CircleDef();
+    sd.density = 1;
+    sd.friction = 1;
+    sd.radius = 10;
+    sd.localPosition.Set(0, 16);
+    bd.AddShape(sd);
+    bd.position.Set(150, 250);
+    player = world.CreateBody(bd);
+    player.speed = 100;
 
     world.SetFilter({
         ShouldCollide: function(a, b) {
@@ -47,22 +48,22 @@ $(function() {
     $('body').append($canvas).keydown(function(e) {
         switch (e.keyCode) {
         case 65:
-            player.way = - 100;
+            player.way = - 1;
             break;
         case 68:
-            player.way = 100;
+            player.way = 1;
             break;
         case 87:
             if (player.canJump) {
                 player.canJump = false;
                 player.way = player.way ? player.way: 0;
                 var v = player.GetLinearVelocity();
-                v.Set(player.way, - 100);
+                v.Set(player.way * player.speed, - player.speed);
             }
             break;
         }
     }).keyup(function(e) {
-        if ([65, 68].indexOf(e.keyCode) >= 0) {
+        if ((e.keyCode === 65 && player.way === - 1) || (e.keyCode === 68 && player.way === 1)) {
             var v = player.GetLinearVelocity();
             v.Set(player.way = 0, v.y);
         }
@@ -75,7 +76,7 @@ $(function() {
         world.Step(1.0 / 60, 1);
         if (player.way) {
             var v = player.GetLinearVelocity();
-            v.Set(player.way, v.y);
+            v.Set(player.way * player.speed, v.y);
         }
         draw.drawWorld(world, ctx);
     },
